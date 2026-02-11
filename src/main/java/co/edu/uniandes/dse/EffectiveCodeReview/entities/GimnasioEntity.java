c4978bb91284b3afe6e6a7f405b4134ec7b2b197
Andrés Rodriguez <af.rodriguezm1@uniandes.edu.co>
2026-02-11 11:57:13 -0500

    Create Gimnasio Entity (#1)

---
+++ src/main/java/co/edu/uniandes/dse/EffectiveCodeReview/entities/GimnasioEntity.java
@@ -0,0 +1,16 @@
+package co.edu.uniandes.dse.EffectiveCodeReview.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class GimnasioEntity extends BaseEntity {
+
+    private String region;
+
+    private String nombre;
+
+    private Boolean completado;
+
+}
